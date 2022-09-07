05f300c8ac85d70e323315b74a583a57b5eacfb9
Szabó Alexandra <alexandra2033@gmail.com>
2022-09-07 16:53:44 +0200

    Utolsó faladatok

--- random_samples.py
+++ random_samples.py
@@ -1,6 +1,16 @@
 # Szimuláljuk a dobókockadobást! 1-6
 
-from random import randint
+from random import choice, randint, shuffle
 
 # randint fgv nem build-in function
-number = randint
+# randint függvény a standard libary része
+number = randint(1, 6)
+print(number)
+
+numbers = [2, 4, 6, 8]
+shuffle(numbers)
+print(numbers)
+
+cards = ["alsó", "felső", "király", "ász"]
+card = choice(cards)
+print(card)
---
+++ strings.py
@@ -0,0 +1,33 @@
+# String egy adatszerkezet, hiszen karakterekből áll
+name = "John Doe"
+for c in name: # végig megy a karaktereken
+    print(c)
+
+# Összeszámolja a névben lévő o betűket
+count = 0
+for c in name:
+    if c == "o":
+        count +=1
+print(count)
+
+print(name[3])
+
+# Szeletelés, slicing
+print(name[1:4])
+
+print(len(name))
+
+print("John" in "John Doe")
+
+if "John" in name:
+    print("Na, ez is egy újabb John")
+
+# Stringeknek vannak saját függvényeik: metódusok
+
+print(name.upper()) # a függvényt a name változón hívjuk meg
+
+fruit = "        alma       "
+print(fruit.strip())
+
+print("john" in "Jack Jane John")
+print("john" in "Jack Jane John".lower())
